ba0071376e944a338991bccac06b20afa378ad40
Peter Gagliardi <pcg34@drexel.edu>
2018-02-12 20:06:57 -0500

    Downsample image
    Now the image is downsampled, taking the average gray value of each
    block.

--- color_by_numbers.py
+++ color_by_numbers.py
@@ -4,6 +4,37 @@ This is a prototype of my color-by-numbers script. I still need to plan this
 out more.
 """
 import cv2
+import numpy
+
+def downsample(image, block_size=32):
+    """
+    Take a grayscale image and downsample. Downsampling is done
+    by averaging blocks of block_size x block_size
+    """
+    # unpack the size of the input image
+    in_rows, in_cols = image.shape
+
+    # Make a buffer for the output image
+    out_rows = in_rows // block_size
+    out_cols = in_cols // block_size
+    result = numpy.zeros((out_rows, out_cols), numpy.uint8)
+
+    # Iterate over the output image
+    for i in range(out_rows):
+        for j in range(out_cols):
+            # Scale to pixels of input image
+            row = i * block_size
+            col = j * block_size
+
+            # Slice out a rectangle from the input image
+            img_slice = image[row:row + block_size, col: col + block_size]
+
+            # Set the output pixel to the average of the block
+            result[i, j] = img_slice.mean()
+
+    # Return the new tiny image
+    return result
+
 
 def main():
     """
@@ -17,6 +48,10 @@ def main():
 
     # Output the grayscale image
     cv2.imwrite('output/gray.png', gray)
+
+    # Downsample the grayscale image
+    small = downsample(gray)
+    cv2.imwrite('output/downsampled.png', small)
 
 if __name__ == '__main__':
     main()
