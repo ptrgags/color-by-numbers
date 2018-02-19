f411bb178890d74b982d3f1088e2d3781768913c
Peter Gagliardi <pcg34@drexel.edu>
2018-02-19 17:50:06 -0500

    Translate circles to PostScript
    Translate the circle parameters into Postscript units.

---
+++ make_circles_printout.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+./shapes_prototype.py > output/circle_fragment.ps
+cat shapes_prototype.ps output/circle_fragment.ps > output/circles_sample.ps
--- shapes_prototype.py
+++ shapes_prototype.py
@@ -52,14 +52,65 @@ def paint_shapes(image):
 
     return result
 
+def shape_params(image):
+    # Unpack the shape of the array, we'll need it in the loop
+    rows, cols = image.shape
+    area = rows * cols
+
+    # Start with big shapes and get progressively smaller
+    radii = [256, 128, 64, 32, 16, 8]
+    for i, radius in enumerate(radii):
+        circle_area = math.pi * radius * radius
+        iterations = int(area // circle_area)
+        #print(f"i={i}, r={radius}, iters={iterations:,}")
+
+        # Make more shapes the smaller the shapes are
+        #iterations = (i + 1) * 5
+        for j in range(iterations):
+            # calculate a random position in the image
+            x = random.randint(0, cols - 1)
+            y = random.randint(0, rows - 1)
+
+            # Make a circle at (x, y) with radius radius
+            # and fetch the average color in that region.
+            # paint a circle with that color in the result
+            # image
+            mask = circle_mask(image, (x, y), radius)
+            avg_color = image[mask].mean()
+            yield ('circle', x, y, radius, avg_color)
+
+def scaling_factor(img):
+    """
+    calculate a scaling factor in points/pixel
+
+    This assumes the image is in landscape orientation and will
+    be mapped to a letter page (portrait) with 1-inch margins.
+    """
+    _, width_pixels = img.shape
+    width_inches = 6.5
+    points_per_inch = 72.0
+    width_points = width_inches * points_per_inch
+    return width_points / width_pixels
+
+def process_shapes(params, img):
+    points_per_pixel = scaling_factor(img)
+    print(points_per_pixel)
+    for shape, x, y, r, avg_color in params:
+        x_points = x * points_per_pixel
+        y_points = y * points_per_pixel
+        r_points = r * points_per_pixel
+        print(f'{x_points:.2f} {y_points:.2f} {r_points:.2f} circle')
+
 
 def main():
     img = cv2.imread('images/gears.jpg')
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # Use a circle mask to select a portion of the image
-    result = paint_shapes(gray)
-    cv2.imwrite('output/circle_gears.png', result)
+    #result = paint_shapes(gray)
+    #cv2.imwrite('output/circle_gears.png', result)
+
+    process_shapes(shape_params(gray), gray)
 
 if __name__ == '__main__':
     main()
